d8b6281954dadea6f05bd90ae8e8110a1e89e6b7
ÁLVARO LAURIÑO RUIZ <alvarolaurino@gmail.com>
2021-12-16 11:04:04 +0100

    no pudo ze el buscado

--- app/src/main/java/Vistas/MainActivity.java
+++ app/src/main/java/Vistas/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 
 import com.example.myapplication.ListAdapter;
@@ -20,18 +22,52 @@ import Entidades.EmpresaTecnologica;
 import Vistas.DetallesEmpresa;
 
 public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView buscador;
     ListView lv;
-    List<Empresa> listaEmpresas;
+    ArrayList<Empresa> listaEmpresas;
+    ArrayList<String> listaNombresEmpresas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv=findViewById(R.id.lvEmpresas);
+        declararElementosVista();
         listaEmpresas = getListaEmpresas();
         ListAdapter listAdapter = new ListAdapter(this, listaEmpresas);
         lv.setAdapter(listAdapter);
         lv.setOnItemClickListener(this::clickListViewElement);
 
+    }
+/*
+    private void setBuscadorOnClickListener() {
+        buscador.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                   String nombreEmpresa = adapterView.get(position);
+                    for (int i = 0; i < listaEmpresas.size(); i++) {
+                        String placeHolderET = (EmpresaTecnologica) listaEmpresas.get(position);
+                        if(nombreEmpresa.equals(placeHolderET.getNombreEmpresa())){
+                            et = (EmpresaTecnologica) listaEmpresas.get(position);
+                        }
+                    }
+                    Intent i = new Intent(getApplicationContext(),DetallesEmpresa.class);
+                    i.putExtra("Empresa",(EmpresaTecnologica) listaEmpresas.get(position));
+                    startActivity(i);
+                }
+            }
+
+        });
+
+
+    private void setBuscadorAdapter() {
+        ArrayAdapter<Empresa> adapterBuscador = new ArrayAdapter<Empresa>(this, android.R.layout.simple_dropdown_item_1line, listaEmpresas);
+        buscador.setAdapter(adapterBuscador);
+    }
+    */
+
+
+    private void declararElementosVista(){
+        buscador = findViewById(R.id.autoCompleteTextView);
+        lv=findViewById(R.id.lvEmpresas);
     }
 
     private void clickListViewElement(AdapterView<?> adapterView, View view, int pos, long id) {
@@ -50,5 +86,13 @@ public class MainActivity extends AppCompatActivity {
         listaEmpresas.add(new EmpresaNoTecnologica(R.drawable.decathlon_logo, "Decathlon", "Deportes", 4764));
         listaEmpresas.add(new EmpresaTecnologica(R.drawable.pc_componentes_logo, "Pc Componentes", "https://www.pccomponentes.com/", "C. de Cavanilles, 35, 28007 Madrid", "pcComponentes@gmail.com",933778866));
         return listaEmpresas;
+    }
+
+    private  ArrayList<String> getListaNombresEmpresa(List<Empresa> listaEmpresas){
+        ArrayList<String> listaNombres = new ArrayList<>();
+        for (Empresa e: listaEmpresas) {
+            listaNombres.add(e.getNombreEmpresa());
+        }
+        return listaNombres;
     }
 }
